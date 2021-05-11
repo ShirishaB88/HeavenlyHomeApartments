82410288f295941e7aea619d9d755b75d9511b8c
ShirishaB88 <shirisha46@gmail.com>
2021-05-11 10:29:42 -0400

    Add project files.

---
+++ HeavenlyHomeApartments_WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HeavenlyHomeApartments_WebMVC.MvcApplication" Language="C#" %>
---
+++ HeavenlyHomeApartments_WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HeavenlyHomeApartments_WebMVC.Startup))]
+namespace HeavenlyHomeApartments_WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
