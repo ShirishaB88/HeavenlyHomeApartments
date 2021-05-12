6de9e590916efda5f231cfd5be2c399baf6f85c4
ShirishaB88 <shirisha46@gmail.com>
2021-05-12 11:44:03 -0400

    Adding Data base migrations, FloorPlan controller and service methods Create and GetFloorPlans

--- HeavenlyHome_Models/FloorPlanModels/FloorPlanCreate.cs
+++ HeavenlyHome_Models/FloorPlanModels/FloorPlanCreate.cs
@@ -1,21 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HeavenlyHome_Models.FloorPlanModels
 {
-    public class FloorPlanListItem
+    public class FloorPlanCreate
     {
-        public int FloorPlanID { get; set; }
+        [Required]
         public int NoOfBeds { get; set; }
+
+        [Required]
+        [Range(1,2)]
         public int NoOfBaths { get; set; }
+
+        [Required]
         public int AreaInSqFt { get; set; }
+
+        [Required]
         public double Price { get; set; }
+
+        [Required]
+        [Range(0,2)]
         public int NoOfGarageSpaces { get; set; }
+
+        [Required]
+        [DataType(DataType.ImageUrl)]
         public string Image { get; set; }
-        public bool IsAvailable { get; set; }      
+
+        [Required]
+        public bool IsAvailable { get; set; }
+
+        [Required]
         public DateTimeOffset CreatedDate { get; set; }
     }
 }
---
+++ HeavenlyHome_Service/FloorPlanService.cs
@@ -0,0 +1,69 @@
+﻿using HeavenlyHome_Data;
+using HeavenlyHome_Models.FloorPlanModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeavenlyHome_Service
+{
+    public class FloorPlanService
+    {
+        private readonly Guid _userID;
+
+        public FloorPlanService(Guid userID)
+        {
+            _userID = userID;
+        }
+
+        public bool CreateFloorPlan(FloorPlanCreate model)
+        {
+              var entity =
+                new FloorPlan()
+                {
+                    UserID = _userID,
+                    NoOfBaths = model.NoOfBaths,
+                    NoOfBeds = model.NoOfBeds,
+                    AreaInSqFt = model.AreaInSqFt,
+                    Price = model.Price,
+                    NoOfGarageSpaces = model.NoOfGarageSpaces,
+                    Image = model.Image,
+                    IsAvailable = model.IsAvailable,
+                    CreatedDate = DateTimeOffset.Now
+
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.FloorPlans.Add(entity);
+               return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<FloorPlanListItem> GetFloorPlans()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .FloorPlans
+                    .Where(e => e.UserID == _userID)
+                    .Select(e => new FloorPlanListItem
+                    {
+                        FloorPlanID = e.FloorPlanID,
+                        NoOfBaths = e.NoOfBaths,
+                        NoOfBeds = e.NoOfBeds,
+                        AreaInSqFt = e.AreaInSqFt,
+                        Price = e.Price,
+                        NoOfGarageSpaces = e.NoOfGarageSpaces,
+                        Image = e.Image,
+                        IsAvailable = e.IsAvailable,
+                        CreatedDate = e.CreatedDate
+
+                    });
+                return query.ToArray();
+            }
+        }
+    }
+}
